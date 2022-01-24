b41140d4116be8886750b0e489764256d95d377a
jiseong <njs8590@naver.com>
2022-01-24 22:45:08 +0900

    retrofit

---
+++ app/src/main/java/com/namdroid/retrofit_sample/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.namdroid.retrofit_sample.utils
+
+object Constants {
+    const val TAG:String = "Log"
+}
+
+object API{
+    const val BASE_URL:String = "https://www.dhlottery.co.kr"
+}
